63f9b52767b99768a3a3d2ae1f857a920dfe8d06
Israelitheballer <gozienwofor@gmail.com>
2026-09-22 23:50:10 +0100

    Created the route table - not assigned to my public subnets yet though

--- network/main.tf
+++ network/main.tf
@@ -85,3 +85,16 @@ resource "aws_internet_gateway" "ashfall_igw" {
     Name = "ashfall-igw"
   }
 }
+
+resource "aws_route_table" "ashfall_public_rt" {
+  vpc_id = aws_vpc.main.id
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ashfall_igw
+  }
+
+  tags = {
+    Name = "ashfall_public_rt"
+  }
+}
